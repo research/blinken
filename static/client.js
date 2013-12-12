8b45dc62fdbce22325bc6d0904baf16aee6664c9
J. Alex Halderman <jhalderm@eecs.umich.edu>
2013-12-12 17:13:32 -0500

    Limit in client

--- static/client.js
+++ static/client.js
@@ -287,20 +287,23 @@ function Bulb() {
     this.b = 0;
     this.g = 0;
     this.a = 0;
+    function limit(x) {
+	return Math.min(1, Math.max(0, x));
+    }
     this.rgb = function (r, g, b) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
+        this.r = limit(r);
+        this.g = limit(g);
+        this.b = limit(b);
         this.a = 1;
     };
     this.rgba = function (r, g, b, a) {
         this.rgb(r, g, b);
-        this.a = a;
+        this.a = limit(a);
     };
     this.cssColor = function () {
-        return 'rgb(' + Math.round(this.a * this.r * 255) + ',' + 
-                        Math.round(this.a * this.g * 255) + ',' + 
-                        Math.round(this.a * this.b * 255) + ')';
+        return 'rgb(' + Math.round(limit(this.a) * limit(this.r) * 255) + ',' + 
+                        Math.round(limit(this.a) * limit(this.g) * 255) + ',' + 
+                        Math.round(limit(this.a) * limit(this.b) * 255) + ')';
     };
 }
 
