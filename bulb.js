74c8cffdd72d4d6f6e67f9a24531e230a071f782
J. Alex Halderman <jhalderm@eecs.umich.edu>
2013-12-12 17:12:06 -0500

    Add limits

--- bulb.js
+++ bulb.js
@@ -3,15 +3,18 @@ function Bulb() {
     this.b = 0;
     this.g = 0;
     this.a = 0;
+    function limit(x) {
+	return Math.min(1, Math.max(0, x));
+    }
     this.rgb = function (r, g, b) {
-        this.r = r;
-        this.g = g;
-        this.b = b;
+        this.r = limit(r);
+        this.g = limit(g);
+        this.b = limit(b);
         this.a = 1;
     };
     this.rgba = function (r, g, b, a) {
         this.rgb(r, g, b);
-        this.a = a;
+        this.a = limit(a);
     };
     this.cssColor = function () {
         return 'rgb(' + Math.round(this.a * this.r * 255) + ',' +
@@ -22,10 +25,10 @@ function Bulb() {
 		if (typeof scale === 'undefined') {
 			scale = 1;
 		}
-		return [Math.round(scale*this.a*255),
-				Math.round(this.r*15),
-				Math.round(this.g*15),
-				Math.round(this.b*15)];
+		return [Math.round(scale*limit(this.a)*255),
+				Math.round(limit(this.r)*15),
+				Math.round(limit(this.g)*15),
+				Math.round(limit(this.b)*15)];
 	};
 }
 
