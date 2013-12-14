6d24877ba03aa68cf9aff1592b12d1f474a5e801
J. Alex Halderman <jhalderm@eecs.umich.edu>
2013-12-14 16:57:21 -0500

    Wait queue wait estimate

--- scheduler.js
+++ scheduler.js
@@ -4,10 +4,11 @@ var crypto = require('crypto'),
 
 var jobs = {};
 var queue = [];
+var stopTime = 0;
 
 exports.makeJob = function(code) {
 	var token = makeToken();
-	jobs[token] = {code: code, cancel: false, status: {} };
+	jobs[token] = {code: code, limit: 120, cancel: false, status: {} };
 	return token;
 };
 
@@ -28,9 +29,40 @@ exports.cancelJob = function(token) {
 	jobs[token].status = {value: 0, message: 'Canceled'};	
 };
 
+function estimateWait(token) {
+	if (typeof jobs[token] === 'undefined') {
+		return;
+	}
+	var wait = stopTime - Date.now()/1000;
+	if (wait < 0) {
+		wait = 0;
+	}
+	for (var i=0; i < queue.length; i++) {
+		var t = queue[i];
+		if (t == token) {
+			break;
+		}
+		if (!jobs[t].cancel) {
+			wait += jobs[t].limit;
+		}
+	}
+	return wait;
+}
+
 exports.getStatus = function(token) {
 	if (typeof jobs[token] === 'undefined') {
 		return undefined;
+	}
+	if (jobs[token].status.value == 10) {
+		// estimate time left in queue
+		var wait = Math.round(estimateWait(token));
+		var m = Math.floor(wait / 60), s = wait - 60*m;
+		var out = m + ':';
+		if (s < 10) {
+			out += '0';
+		}
+		out += s;
+		jobs[token].status.message = 'Queued (' + out + ')';
 	}
 	return jobs[token].status;
 };
@@ -56,9 +88,10 @@ function scheduler() {
 		}
 		jobs[token].status = {value: 20, message: 'Running'};
 		console.log(jobs[token].code);
+		stopTime = Date.now()/1000 + jobs[token].limit;
 		return runner.run({ // User program
 			code: jobs[token].code,
-			limit: 120,
+			limit: jobs[token].limit,
 			cancel: function() { return jobs[token].cancel },				  
 			after: function(status, message) {
 				if (status == 0) {
