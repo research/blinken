67fa99c7086350421b676c25919f9c73c417af5b
J. Alex Halderman <jhalderm@eecs.umich.edu>
2013-12-14 20:21:26 -0500

    API improvements

--- bulb.js
+++ bulb.js
@@ -2,12 +2,18 @@
 // Class to represent and manipulate a bulb
 //
 function Bulb(w,x,y,z) {
+    // Reset bulb
+    this.clear = function() {
+        this.r = this.g = this.b = 0;
+        this.a = 1;
+    };
+
     // Create a Bulb:
     //   new Bulb()
     //   new Bulb(existingBulb)
     //   new Bulb(r,g,b)
     //   new Bulb(r,g,b,a)
-    this.r = this.g = this.b = this.a = 0;
+    this.clear();
     if (typeof w === 'object' && w instanceof Bulb &&
         typeof x === 'undefined' && typeof y === 'undefined' && typeof z === 'undefined') {
         this.r = limit(w.r);
@@ -21,14 +27,14 @@ function Bulb(w,x,y,z) {
         if (typeof z === 'number') {
             this.a = z;
         }
-    }
+    }    
 
-    // You can color channels and overall brightness ("alpha") directly.
-    // Range is 0-1.
-    // this.r = 0;
-    // this.g = 0;
-    // this.b = 0;
-    // this.a = 0;
+    // You can set color channels and overall brightness ("alpha") directly.
+    // Range is 0-1:
+    //   this.r = 0;
+    //   this.g = 0;
+    //   this.b = 0;
+    //   this.a = 1;
 
     // Set light color: rgb(r,g,b)
     this.rgb = function (r, g, b) {
@@ -36,26 +42,45 @@ function Bulb(w,x,y,z) {
         this.g = limit(g);
         this.b = limit(b);
         this.a = 1;
+        return this;
     };
 
     // Set light color with alpha: rgba(r,g,b,a)
     this.rgba = function (r, g, b, a) {
-        this.rgb(r, g, b);
         this.a = limit(a);
+        return this.rgb(r, g, b);
     };
 
-    // Set bulb to off
-    this.off = function() {
-        this.r = this.g = this.b = this.a = 0;
-    }
+    // Add color of another bulb to this one
+    this.add = function(bulb) {
+        this.r = limit(this.r + bulb.r);
+        this.g = limit(this.g + bulb.g);
+        this.b = limit(this.b + bulb.b);
+        this.a = limit(this.a + bulb.a);
+        return this;
+    };
+
+    // Convenience functions for simple colors
+    this.black  = function() { this.r = 0; this.g = 0; this.b = 0; return this; };
+    this.red    = function() { this.r = 1; this.g = 0; this.b = 0; return this; };
+    this.green  = function() { this.r = 0; this.g = 1; this.b = 0; return this; };
+    this.blue   = function() { this.r = 0; this.g = 0; this.b = 1; return this; };
+    this.cyan   = function() { this.r = 0; this.g = 1; this.b = 1; return this; };
+    this.purple = function() { this.r = 1; this.g = 0; this.b = 1; return this; };
+    this.yellow = function() { this.r = 1; this.g = 1; this.b = 0; return this; };
+    this.white  = function() { this.r = 1; this.g = 1; this.b = 1; return this; };
 
     ////////////////////////////////////////////////////////////
+    // Internal stuff
+
     this.gamma = 0.3; // scale color in simulator to match bulbs
+
     this.cssColor = function () {
         return 'rgb(' + Math.round(Math.pow(limit(this.a) * limit(this.r), this.gamma) * 255) + ',' + 
                         Math.round(Math.pow(limit(this.a) * limit(this.g), this.gamma) * 255) + ',' + 
                         Math.round(Math.pow(limit(this.a) * limit(this.b), this.gamma) * 255) + ')';
     };
+
     function limit(x) {
 	return Math.min(1, Math.max(0, x));
     }
